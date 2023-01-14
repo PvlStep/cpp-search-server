b6488aee7356041dbe1569b26a8d5496b0eb4481
pvlstep <pvlstep@gmail.com>
2023-01-14 21:10:21 +0300

    Спринт 5. Итоговый проект. Исправлены замечания от 14.01.2023.

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,22 +1,23 @@
 #include "request_queue.h"
 
-    int RequestQueue::GetNoResultRequests() const {
-        int empty_requests = 0;
-        for (QueryResult result_ : requests_) {
-            if (result_.responce_ == 0) {
-                ++empty_requests;
-            }
+int RequestQueue::GetNoResultRequests() const {
+    int empty_requests = 0;
+    for (QueryResult result_ : requests_) {
+        if (result_.responce_ == 0) {
+            ++empty_requests;
         }
-        return empty_requests;
     }
+    return empty_requests;
+}
 
-    int RequestQueue::TotalRequets() const {
-        return requests_.size();
+
+int RequestQueue::TotalRequets() const {    
+    return requests_.size();
+}
+
+
+void RequestQueue::DequeRequests() {
+    if (requests_.size() > min_in_day_) {
+        requests_.pop_front();
     }
-
-    void RequestQueue::DequeRequests() {
-        if (requests_.size() > min_in_day_) {
-            requests_.pop_front();
-        }
-
-    }
+}
